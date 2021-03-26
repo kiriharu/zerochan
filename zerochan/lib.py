a495852aca2988ba5aaf3769a1c0199c80a88e4f
kiriharu <kiriharu@yandex.ru>
2021-03-26 23:23:17 +0300

    fix attr and page getting, rewrite args setting

--- zerochan/lib.py
+++ zerochan/lib.py
@@ -18,24 +18,27 @@ class ZeroChan:
 
     def __init__(self):
         self._tag = ""
+        self._page = 1
+        self._sort_by = SortBy.LAST
+        self._size = PictureSize.ALL_SIZES
         self.cookies = dict(
             z_lang="en"
         )
         self.req_args = {}
 
     def size(self, size: PictureSize) -> 'ZeroChan':
-        self.req_args.update(dict(d=int(size)))
+        self._size = size
         return self
 
     def sort(self, sort_by: SortBy) -> 'ZeroChan':
-        self.req_args.update(dict(s=sort_by))
+        self._sort_by = sort_by
         return self
 
     def page(self, page_num: int) -> 'ZeroChan':
-        self.req_args.update(dict(p=page_num))
+        self._page = page_num
         return self
 
-    def tag(self, title: str):
+    def tag(self, title: str) -> 'ZeroChan':
         self._tag = title
         return self
 
@@ -47,11 +50,17 @@ class ZeroChan:
         return self
 
     def _get_soup(self):
+        self.req_args.update(dict(
+            p=self._page,
+            s=self._sort_by,
+            d=int(self._size)
+        ))
         req = requests.get(
             f"{self.WEBSITE_URL}/{self._tag}",
             params=self.req_args,
             cookies=self.cookies
         )
+        #print(req.url)
         return BeautifulSoup(req.content, "html.parser")
 
     def category(self) -> ZeroChanCategory:
@@ -74,8 +83,11 @@ class ZeroChan:
     def _parse_pics(self, pics_soup) -> List[ZeroChanImage]:
         images = []
         for pic in pics_soup.find_all("li"):
-
-            title = pic.img.get("title")
+            if getattr(pic.a, "img") is None:
+                # parser try to parse "Members only" link if not authorized
+                # idk why it's going do
+                continue
+            title = pic.a.img.get("title")
             splitted_title = title.split(" ")
             size = int(splitted_title[1][:-2])  # remove "kb" chars
             height, width = map(int, list(splitted_title[0].split("x")))  # split by x and convert to int
@@ -94,19 +106,24 @@ class ZeroChan:
     def pics(self) -> ZeroChanPage:
         soup = self._get_soup()
         pics_soup = soup.find("ul", dict(id="thumbs2"))
+
         if pics_soup is None:
             raise NoPicturesFound
         imgs = self._parse_pics(pics_soup)
 
         # Setting page and maxpage to 1 if random mode chosen
-        if self.req_args.get("s") == SortBy.RANDOM:
+        if self._sort_by == SortBy.RANDOM:
             page = 1
             max_page = 1
         else:
             paginator_el = soup.find("p", {"class": "pagination"})
             str_list = paginator_el.text.strip().replace("\t", " ").split(" ")
-            page = int(str_list[1])
-            max_page = int(str_list[3])
+            if self._page == 1:
+                page = int(str_list[1])
+                max_page = int(str_list[3])
+            else:
+                page = int(str_list[4])
+                max_page = int(str_list[6])
         return ZeroChanPage(
             images=imgs,
             page=page,
